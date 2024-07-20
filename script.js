e143f9bc5bb63dbd77f3d6e7ded469c4f1f699b8
Jyo (she/her) <75916600+jyotsna-iy@users.noreply.github.com>
2024-07-20 15:05:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ fetch('nav.html')
   .then(data => {
     document.getElementById('nav-placeholder').innerHTML = data;
   });
- 
+
+function toggleMenu() {
+    var sidenav = document.getElementById("mySidenav");
+    sidenav.classList.toggle("show");
+}
+
+function toggleSubmenu(event, element) {
+    event.preventDefault();
+    element.classList.toggle("submenu-active");
+}
